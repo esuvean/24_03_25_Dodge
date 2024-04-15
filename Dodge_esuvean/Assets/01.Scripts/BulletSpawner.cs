68b6ac4b3de7dcca75f76df60e6814f691d79124
esuvean <esuvean@naver.com>
2024-04-15 12:34:49 +0900

    [Feat] 닷지게임

---
+++ Dodge_esuvean/Assets/01.Scripts/BulletSpawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bulletPrefeb;
+    public float spawnRateMin = 0.5f;
+    public float spawnRateMax = 3f;
+
+    private Transform target;
+    private float spawnRate;
+    private float TimeAfterSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeAfterSpawn = 0;
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        target = FindObjectOfType<Player_controller>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeAfterSpawn += Time.deltaTime;
+        if ( TimeAfterSpawn >= spawnRate)
+        {
+            TimeAfterSpawn = 0f;
+            GameObject bullet = Instantiate(bulletPrefeb, transform.position,transform.rotation);
+            bullet.transform.LookAt(target);
+        }
+        
+    }
+}
